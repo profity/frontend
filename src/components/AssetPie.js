b8380796cc097ff1087eb6eeae4751b57739fcde
kidylee <kidylee@gmail.com>
2018-02-06 09:32:11 -0500

    add dataKey

--- src/components/AssetPie.js
+++ src/components/AssetPie.js
@@ -10,9 +10,9 @@ const data02 = [{name: 'Group A', value: 2400}, {name: 'Group B', value: 4567},
                   {name: 'Group E', value: 3908}, {name: 'Group F', value: 4800}];
 
 const AssetPie = ()=>(
-    <PieChart width={800} height={400}>
-        <Pie isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={80} fill="#8884d8" label/>
-        <Pie data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/>
+    <PieChart  width={800} height={400}>
+        <Pie dataKey="value" isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={80} fill="#8884d8" label/>
+        <Pie dataKey="value" data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/>
         <Tooltip/>
     </PieChart>
 )
