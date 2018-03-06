e5d1e7169c3eca46d4f9ceb3db0d42fc380ce988
kidylee <kidylee@gmail.com>
2018-03-06 15:24:14 -0500

    add editorconfig

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,24 @@ import React from 'react';
 import './App.css'
 import withRoot from '../withRoot';
 import FAB from '../containers/FAB';
+import Paper from 'material-ui/Paper';
+
+const style = {
+  height: 100,
+  width: '50%',
+  margin: 20,
+  textAlign: 'center',
+  display: 'inline-block'
+};
 
 const App = () => (
+  <div>
+    <Paper style={style} zDepth={1}/>
+    <Paper style={style} zDepth={1}/>
+    <Paper style={style} zDepth={1}/>
+    <Paper style={style} zDepth={1}/>
     <FAB/>
+  </div>
 )
 
 export default withRoot(App);
